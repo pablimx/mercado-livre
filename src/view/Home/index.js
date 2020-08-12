3c1ff9b9ccc305021a9be17626effe35b3c3ef35
Pablo Resende <pablim@live.co.uk>
2020-08-12 18:58:29 -0300

    incluindo logo + botoes

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { View, Text, Image, TextInput } from "react-native";
-
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import Button from "../../components/Button";
+import logo from "../../../assets/logo.png";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Image />
+      <Image source={logo} />
+      <Button style="outline">
+        <Text>Teste</Text>
+      </Button>
+      <Button>
+        <Text>Teste</Text>
+      </Button>
     </View>
   );
 }
